3d108f5e6bc3b7ca660e50e7622da6c9df42ad24
RomeroSupero <129064670+RomeroSupero@users.noreply.github.com>
2023-04-04 16:19:51 +0300

    Add files via upload

---
+++ js3.js
@@ -0,0 +1,93 @@
+// 1
+// let firstNum = Number(prompt('A'));
+// let secondNum = Number(prompt('B'));
+// let result = 0;
+
+// for (let i = firstNum ; i < secondNum; i++){
+//   result +=i;
+// }
+//   console.log(result);
+
+
+//  //2
+// for (let i = 10 ; i > 0 ; i--) {
+//   console.log(i);
+// }
+
+
+// 3 
+
+// let lines = Number(prompt('size'));
+// let lattice = ''; 
+// let lett = "#";
+
+//  for(let i=0;i<lines;i++){
+//     lattice+=lett
+//     console.log(lattice);
+//    }
+   
+// 4
+
+
+
+// 5
+// let dayWeek = prompt("Days of the week: ");
+
+// switch (dayWeek) {
+//     case "Monday":
+//     case "monday":
+//     case "Wednesday":
+//     case "wednesday":
+//     case "Friday":
+//     case "friday":
+//     case "Saturday":
+//     case "saturday":
+//     case "Sunday":
+//     case "sunday":
+//         console.log('Do you have a day off');
+//         break;
+//     case "Tuesday":
+//     case "tuesday":
+//     case "Thursday":
+//     case "thursday":
+//         console.log('You have classes today');
+//         break;
+//     default:
+//         alert('Error');
+//         console.log('Error');
+//         break;
+// }
+
+
+// 6
+//     let planetSolarSystem  = prompt('Planet');
+
+//     switch (planetSolarSystem) {
+//        case "Mercury" :
+//        case "Venus":
+//        case "Earth" :
+//        case "Mars":
+//        case "Jupiter":
+//        case "Saturn":
+//        case "Uranus":
+//        case "Neptune":
+//             alert('Yes');
+//             console.log('Yes')
+//         break;
+//     default:
+//         alert('Error');
+//         console.log('Error');
+//         break;
+//     }
+
+// 7
+
+// let smile = [':)', '=)',':)', '=)',':)', '=)'];
+
+// for(let i = 0; i < smile.length; i++){
+//    if(smile[i] === '=)'){
+//     smile[i] = ';)'
+//    } 
+   
+// }
+// console.log(smile);
